04e06314f6cb9000abeaff14fa7e442d515c5702
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-04-24 14:49:26 -0300

    add middlwware de erro

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import express from 'express';
+import errorHandler from './middlewares/errorHandler';
 
 const app = express();
 
 app.use(express.json());
+
+// rotas
+
+// middleware de error
+app.use(errorHandler);
 
 export default app;
