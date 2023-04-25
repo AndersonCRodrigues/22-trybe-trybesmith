a6d3b14f02dfa8da1d81fb164fc4a66e8886f04a
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-04-25 08:21:43 -0300

    refactor remove console.log

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -18,7 +18,6 @@ export default class UserService {
 
   login = async (username: string, password: string) => {
     const result = await this.model.login(username);
-    console.log(result);
 
     if (!result || result.password !== password) {
       const erro = { status: 401, message: 'Username or password invalid' };
